def8f3b4a43dbc5e1d7901539763dc03ca05ab1f
Bima Saputra <109858732+bmsptra24@users.noreply.github.com>
2026-02-20 10:25:03 +0700

    feat: smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Route, Routes } from "react-router";
 import LandingPage from "./pages/landing-page";
+import GalleryPage from "./pages/gallery-page";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/carpet" element={<GalleryPage />} />
     </Routes>
   );
 }
