7f94cf86d93b7629b55455a8a20fde6627f80e3c
Bima Saputra <109858732+bmsptra24@users.noreply.github.com>
2026-04-13 16:20:49 +0700

    fix: type error

--- src/pages/detail-service.tsx
+++ src/pages/detail-service.tsx
@@ -21,7 +21,7 @@ const DetailService: React.FC = () => {
     initial: { opacity: 0, y: 30 },
     whileInView: { opacity: 1, y: 0 },
     viewport: { once: true },
-    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as const },
   };
 
   return (
