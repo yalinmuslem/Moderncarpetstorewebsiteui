15c7f78061a8db93b2a178c90332983fc029a44a
yalinulloh <yalin.muslem@gmail.com>
2026-05-09 11:51:39 +0700

    fix: remove leading whitespace from phoneNumber string in contact config

--- src/configs/contact.ts
+++ src/configs/contact.ts
@@ -1,7 +1,7 @@
 export const siteContact = {
   whatsappNumber: " +447958057867",
   whatsappMessage: "Hello! I am interested in your product.",
-  phoneNumber: " +447958057867",
+  phoneNumber: "+447958057867",
   address: "Leyton, London, E10",
   googleMapsUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2480.252062534062!2d-0.015241423405785041!3d51.56360697182583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761d7639556857%3A0xc0747447477382d3!2sLeyton%2C%20London!5e0!3m2!1sen!2suk!4v1711234567890!5m2!1sen!2suk",
   email: "standardcarpetsltd@hotmail.co.uk",
